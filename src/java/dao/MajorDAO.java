25bc10185a25e95a9abc4e21fb82969e3d0aca63
An Mai <maiduyan2005@gmail.com>
2026-06-22 01:55:51 +0700

    Add SQLException in majorDAO

--- src/java/dao/MajorDAO.java
+++ src/java/dao/MajorDAO.java
@@ -1,13 +1,13 @@
 package dao;
 
 import dal.DBContext;
-import model.Major;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.Major;
 
 public class MajorDAO {
 
